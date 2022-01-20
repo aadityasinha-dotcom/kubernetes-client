09d01c1c946160b9cb552606f173629f91b55258
aadityasinha-dotcom <aadityasinha009@gmail.com>
2022-01-20 15:24:37 +0530

    replace lambda with a method reference for better readability

--- crd-generator/api/src/main/java/io/fabric8/crd/generator/AbstractCustomResourceHandler.java
+++ crd-generator/api/src/main/java/io/fabric8/crd/generator/AbstractCustomResourceHandler.java
@@ -75,7 +75,7 @@ public abstract class AbstractCustomResourceHandler {
     Map<String, Property> additionalPrinterColumns = new HashMap<>(additionalPrinterColumnDetector.getProperties());
     additionalPrinterColumns.forEach((path, property) -> {
       Map<String, Object> parameters = property.getAnnotations().stream()
-      .filter(a -> a.getClassRef().getName().equals("PrinterColumn")).map(AnnotationRef::getParameters)
+      .filter(AnnotationRef::getClassRef().getName().equals("PrinterColumn")).map(AnnotationRef::getParameters)
         .findFirst().orElse(Collections.emptyMap());
       String type = AbstractJsonSchema.getSchemaTypeFor(property.getTypeRef());
       String column = (String) parameters.get("name");
